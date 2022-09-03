e9101ddb638d6f5acf2480b335bb2d77d04b0ce5
ahalekelly <ahalekelly@gmail.com>
2022-09-03 05:38:11 -0700

    state machine prototype

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,26 @@
 #include "ESP32Servo.h"
 
 // Configuration Variables
-uint16_t revThrottle = 1999; // scale is 0 - 1999
-uint16_t idleThrottle = 50; // scale is 0 - 1999
-uint32_t idleTime = 4000; // ms
+char wifiSsid[32] = "ssid";
+char wifiPass[63] = "pass";
+uint32_t revRPM = 50000;
+uint32_t idleRPM = 1000;
+uint32_t idleTime_ms = 5000;
+uint32_t scaledMotorKv = 2550 * 11; // motor kv * battery voltage divider ratio
+uint8_t burstLength = 3;
+bool closedLoopControl = false;
+uint16_t firingDelay_ms = 200;
+uint16_t spindownSpeed = 1;
 bool revSwitchNormallyClosed = false; // should we invert rev signal?
 bool triggerSwitchNormallyClosed = false;
 bool cycleSwitchNormallyClosed = false;
 uint16_t debounceTime = 50; // ms
-char wifiSsid[32] = "ssid";
-char wifiPass[63] = "pass";
 
 // Advanced Configuration Variables
 char AP_SSID[32] = "Dettlaff";
 char AP_PW[32] = "KellyIndu";
+dshot_mode_t dshotMode =  DSHOT300; // DSHOT_OFF to fall back to servo PWM
+uint16_t targetLoopTime_us = 1000; // microseconds
 
 typedef struct {
   int8_t revSwitch;
@@ -33,9 +40,10 @@ typedef struct {
   int8_t esc4;
   int8_t telem;
   int8_t button;
+  int8_t batteryADC;
 } Pins_t;
 
-const Pins_t pins_v0_3 = {
+const Pins_t pins_v0_3_n20 = {
   .revSwitch = 15,
   .triggerSwitch = 32,
   .cycleSwitch = 23,
@@ -48,6 +56,20 @@ const Pins_t pins_v0_3 = {
   .esc4 = 17,
   .telem = 16,
   .button = 0,
+  .batteryADC = 33,
+};
+
+const Pins_t pins_v0_3_noid = {
+  .revSwitch = 15,
+  .triggerSwitch = 32,
+  .pusher = 2,
+  .esc1 = 19,
+  .esc2 = 18,
+  .esc3 = 5,
+  .esc4 = 17,
+  .telem = 16,
+  .button = 0,
+  .batteryADC = 33,
 };
 
 const Pins_t pins_v0_2 = {
@@ -58,6 +80,7 @@ const Pins_t pins_v0_2 = {
   .esc4 = 17,
   .telem = 16,
   .button = 0,
+  .batteryADC = 12,
 };
 
 const Pins_t pins_v0_1 = {
@@ -68,14 +91,32 @@ const Pins_t pins_v0_1 = {
   .esc4 = 13,
 };
 
-Pins_t pins = pins_v0_3;
-dshot_mode_t dshotMode =  DSHOT300; // DSHOT_OFF to fall back to servo PWM
-uint16_t loopTime = 1000; // microseconds
+Pins_t pins = pins_v0_3_n20;
 // End Configuration Variables
 
-uint32_t loopStartTime = micros();
-uint32_t prevTime = micros();
-uint16_t throttleValue = 0; // scale is 0 - 1999
+// state machine diagram https://drive.google.com/file/d/1OglAILRt0AgflKg7DCO_BhukITWqTDnz/view?usp=sharing
+enum state_t { // flywheel state, then pusher state
+  STATE_IDLE,
+  STATE_ACCELERATING_STANDBY,
+  STATE_ACCELERATING_WAITING, // ACCELERATING = wheels not yet at full speed
+  STATE_REV_STANDBY, // REV = wheels at full speed
+  STATE_REV_FIRING,
+  STATE_REV_RESET, // RESET = trigger is held down in semi or burst mode and cycle has completed
+  STATE_IDLE_RESET
+};
+
+state_t state = STATE_IDLE;
+
+uint32_t loopStartTimer_us = micros();
+uint16_t loopTime_us = targetLoopTime_us;
+uint32_t time_ms = millis();
+int32_t lastRevTime_ms = -100000; // for calculating idling
+uint32_t targetRPM = 0;
+uint32_t throttleValue = 0; // scale is 0 - 1999
+uint16_t batteryADC_mv = 0; // voltage at the ADC, after the voltage divider
+uint8_t shotsToFire = 0;
+
+const uint32_t maxThrottle = 1999;
 
 Bounce2::Button revSwitch = Bounce2::Button();
 Bounce2::Button triggerSwitch = Bounce2::Button();
@@ -143,8 +184,8 @@ void setup() {
 }
 
 void loop() {
-  prevTime = loopStartTime;
-  loopStartTime = micros();
+  loopStartTimer_us = micros();
+  time_ms = millis();
   if (pins.revSwitch) {
     revSwitch.update();
   }
@@ -154,23 +195,73 @@ void loop() {
   if (pins.triggerSwitch) {
     triggerSwitch.update();
   }
-  if (loopStartTime > 5000000) { // for first 5s, send min throttle so ESCs can boot & arm
-    if (pins.triggerSwitch) {
+
+  switch (state){
+    case STATE_IDLE:
       if (triggerSwitch.isPressed()) {
-        digitalWrite(pins.pusher, HIGH);
-        digitalWrite(pins.pusherBrake, LOW);
-      } else {
-        digitalWrite(pins.pusher, HIGH);
-        digitalWrite(pins.pusherBrake, HIGH);
+        targetRPM = revRPM;
+        lastRevTime_ms = time_ms;
+        state = STATE_ACCELERATING_WAITING;
+      } else if (revSwitch.isPressed()) {
+        targetRPM = revRPM;
+        lastRevTime_ms = time_ms;
+        state = STATE_ACCELERATING_STANDBY;
+      } else if (time_ms < lastRevTime_ms+idleTime_ms) { // idle flywheels
+        targetRPM = idleRPM;
+      } else { // stop flywheels
+        targetRPM = 0;
       }
-    }
-    if (revSwitch.isPressed()) {
-      throttleValue = revThrottle;
-    } else if (revSwitch.currentDuration() < idleTime) {
-      throttleValue = idleThrottle;
-    } else {
-      throttleValue = 0;
-    }
+      break;
+    case STATE_ACCELERATING_STANDBY:
+      if (!closedLoopControl && time_ms > lastRevTime_ms + firingDelay_ms) {
+        state = STATE_REV_STANDBY;
+        if (triggerSwitch.isPressed()) {
+          state =  STATE_REV_FIRING;
+        }
+      } else if (triggerSwitch.isPressed()) {
+        state = STATE_ACCELERATING_WAITING;
+      }
+      break;
+    case STATE_ACCELERATING_WAITING:
+      if (!closedLoopControl && time_ms > lastRevTime_ms + firingDelay_ms) {
+        state = STATE_REV_FIRING;
+      }
+      break;
+    case STATE_REV_STANDBY:
+      if (triggerSwitch.isPressed()) {
+        state = STATE_REV_FIRING;
+      } else if (!revSwitch.isPressed()) {
+        state = STATE_IDLE;
+      }
+      break;
+    case STATE_REV_FIRING:
+      if (!triggerSwitch.isPressed() && cycleSwitch.isPressed()) {
+        if (revSwitch.isPressed()) {
+          state = STATE_REV_STANDBY;
+        } else {
+          state = STATE_IDLE;
+        }
+      }
+      break;
+    case STATE_REV_RESET:
+      break;
+    case STATE_IDLE_RESET:
+      break;
+  }
+
+  if (state == STATE_REV_FIRING) {
+    digitalWrite(pins.pusher, HIGH);
+    digitalWrite(pins.pusherBrake, LOW);
+  } else {
+    digitalWrite(pins.pusher, HIGH);
+    digitalWrite(pins.pusherBrake, HIGH);
+  }
+
+  if (closedLoopControl) {
+    // ray control code goes here
+  } else {
+    throttleValue = max(maxThrottle * targetRPM / batteryADC_mv * 1000 / scaledMotorKv,
+    throttleValue-spindownSpeed);
   }
 
   // send signal to ESCs
@@ -185,9 +276,14 @@ void loop() {
     dshot3.send_dshot_value(throttleValue+48, NO_TELEMETRIC);
     dshot4.send_dshot_value(throttleValue+48, NO_TELEMETRIC);
   }
-//  Serial.println(loopStartTime - prevTime);
   ArduinoOTA.handle();
-  delayMicroseconds(max((long)(0), (long)(loopTime-(micros()-loopStartTime))));
+  loopTime_us = micros() - loopStartTimer_us;
+  if (loopTime_us > targetLoopTime_us) {
+    Serial.print("loop over time, ");
+    Serial.println(loopTime_us);
+  } else {
+    delayMicroseconds(max((long)(0), (long)(targetLoopTime_us-loopTime_us)));
+  }
 }
 
 void WiFiInit() {
@@ -201,10 +297,18 @@ void WiFiInit() {
     Serial.print("WiFi Connected ");
     Serial.println(wifiSsid);
     ArduinoOTA.setHostname("Dettlaff");
+/*
+    if(!MDNS.begin("dettlaff")) {
+      Serial.println("Error starting mDNS");
+      return;
+    }
+*/    
+    Serial.println(WiFi.localIP());
   
     // No authentication by default
     // ArduinoOTA.setPassword("admin");
-    
+  }
+  
     ArduinoOTA
     .onStart([]() {
       String type;
@@ -237,4 +341,3 @@ void WiFiInit() {
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
   }
-}
