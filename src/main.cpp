4a8c6ae2d4e17b9160f6940381552bfb3cc1f2c6
Erik Miller <erikm95@me.com>
2022-12-25 19:14:49 -0800

    Commenting out the wifi ota to make room for BLE in flash
    We really probably should optimize this more later

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,7 @@ DShotRMT dshot2(pins.esc2, RMT_CHANNEL_2);
 DShotRMT dshot3(pins.esc3, RMT_CHANNEL_3);
 DShotRMT dshot4(pins.esc4, RMT_CHANNEL_4);
 
-void WiFiInit();
+// void WiFiInit();
 
 void setup()
 {
@@ -101,7 +101,7 @@ void setup()
     pinMode(pins.flywheel, OUTPUT);
     digitalWrite(pins.flywheel, HIGH);
   }
-  WiFiInit();
+  // WiFiInit();
   if (pins.revSwitch)
   {
     revSwitch.attach(pins.revSwitch, INPUT_PULLUP);
@@ -333,60 +333,60 @@ void loop()
   shell.executeIfInput();
 }
 
-void WiFiInit()
-{
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(wifiSsid, wifiPass);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED)
-  {
-    Serial.println("WiFi Connection Failed!");
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(AP_SSID, AP_PW);
-  }
-  else
-  {
-    Serial.print("WiFi Connected ");
-    Serial.println(wifiSsid);
-    ArduinoOTA.setHostname("Dettlaff");
-    /*
-        if(!MDNS.begin("dettlaff")) {
-          Serial.println("Error starting mDNS");
-          return;
-        }
-    */
-    Serial.println(WiFi.localIP());
+// void WiFiInit()
+// {
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(wifiSsid, wifiPass);
+//   if (WiFi.waitForConnectResult() != WL_CONNECTED)
+//   {
+//     Serial.println("WiFi Connection Failed!");
+//     WiFi.mode(WIFI_AP);
+//     WiFi.softAP(AP_SSID, AP_PW);
+//   }
+//   else
+//   {
+//     Serial.print("WiFi Connected ");
+//     Serial.println(wifiSsid);
+//     ArduinoOTA.setHostname("Dettlaff");
+//     /*
+//         if(!MDNS.begin("dettlaff")) {
+//           Serial.println("Error starting mDNS");
+//           return;
+//         }
+//     */
+//     Serial.println(WiFi.localIP());
 
-    // No authentication by default
-    // ArduinoOTA.setPassword("admin");
-  }
+//     // No authentication by default
+//     // ArduinoOTA.setPassword("admin");
+//   }
 
-  ArduinoOTA
-      .onStart([]()
-               {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH) {
-        type = "sketch";
-      } else { // U_SPIFFS
-        type = "filesystem";
-      }
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type); })
-      .onEnd([]()
-             { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+//   ArduinoOTA
+//       .onStart([]()
+//                {
+//       String type;
+//       if (ArduinoOTA.getCommand() == U_FLASH) {
+//         type = "sketch";
+//       } else { // U_SPIFFS
+//         type = "filesystem";
+//       }
+//       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+//       Serial.println("Start updating " + type); })
+//       .onEnd([]()
+//              { Serial.println("\nEnd"); })
+//       .onProgress([](unsigned int progress, unsigned int total)
+//                   { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+//       .onError([](ota_error_t error)
+//                {
+//       Serial.printf("Error[%u]: ", error);
+//       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+//       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+//       else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+//       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+//       else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
 
-  ArduinoOTA.begin();
+//   ArduinoOTA.begin();
 
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-}
+//   Serial.println("Ready");
+//   Serial.print("IP address: ");
+//   Serial.println(WiFi.localIP());
+// }
