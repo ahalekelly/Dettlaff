f00e800f3d21fb0a144ecf53efd7eb32b58b5573
ahalekelly <ahalekelly@gmail.com>
2023-08-15 17:03:19 -0700

    add pwm support for at8870 pusher driver

--- src/at8870Driver.cpp
+++ src/at8870Driver.cpp
@@ -1,34 +1,42 @@
 #include "at8870Driver.h"
 
+const uint8_t ledc_channel = 0;
+const uint32_t ledc_max_frequency = 24000;
+const uint8_t ledc_resolution = 10;
+
+const uint32_t ledc_max_value = pow(2, ledc_resolution) - 1;
+
 At8870::At8870(uint8_t pin1, uint8_t pin2, uint32_t pwmFrequency)
 {
-    m_pin1 = pin1;
-    m_pin2 = pin2;
-    pinMode(m_pin1, OUTPUT);
-    digitalWrite(m_pin1, LOW);
-    pinMode(m_pin2, OUTPUT);
-    digitalWrite(m_pin2, LOW);
+    ledcSetup(1, min(ledc_max_frequency, pwmFrequency), ledc_resolution);
+    ledcAttachPin(pin1, 1);
+    ledcWrite(1, 0);
+
+    ledcSetup(2, min(ledc_max_frequency, pwmFrequency), ledc_resolution);
+    ledcAttachPin(pin2, 2);
+    ledcWrite(2, 0);
 }
 
 void At8870::drive(float dutyCycle, bool reverseDirection) // dutyCycle is ignored - always runs full speed
 {
+    uint32_t m_dutyCycleInt = min(ledc_max_value, max((uint32_t)0, static_cast<uint32_t>(dutyCycle * ledc_max_value / 100)));
     if (reverseDirection) {
-        digitalWrite(m_pin1, LOW);
-        digitalWrite(m_pin2, HIGH);
+        ledcWrite(1, 0);
+        ledcWrite(2, m_dutyCycleInt);
     } else {
-        digitalWrite(m_pin1, HIGH);
-        digitalWrite(m_pin2, LOW);
+        ledcWrite(1, m_dutyCycleInt);
+        ledcWrite(2, 0);
     }
 }
 
-void At8870::brake()
+void At8870::brake() // brake and coast are swapped for the DRV8243
 {
-    digitalWrite(m_pin1, HIGH);
-    digitalWrite(m_pin2, HIGH);
+    ledcWrite(1, ledc_max_value); // TODO: check on scope that this actually is full on
+    ledcWrite(2, ledc_max_value);
 }
 
 void At8870::coast()
 {
-    digitalWrite(m_pin1, LOW);
-    digitalWrite(m_pin2, LOW);
+    ledcWrite(1, 0);
+    ledcWrite(2, 0);
 }
